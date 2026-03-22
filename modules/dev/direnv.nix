e9704623cae378e0c4ac94d93aec95d1d27ec63a
Horace Liu <im.liuhq@gmail.com>
2026-03-22 13:08:27 +0800

    migrate(angrr): config

--- modules/dev/direnv.nix
+++ modules/dev/direnv.nix
@@ -21,7 +21,39 @@ in
 
     services.angrr = {
       enable = true;
-      period = "14d";
+      settings = {
+        temporary-root-policies = {
+          direnv = {
+            path-regex = "/\\.direnv/";
+            period = "14d";
+          };
+          result = {
+            path-regex = "/result[^/]*$";
+            period = "3d";
+          };
+        };
+        profile-policies = {
+          system = {
+            profile-paths = [ "/nix/var/nix/profiles/system" ];
+            keep-since = "14d";
+            keep-latest-n = 5;
+            keep-booted-system = true;
+            keep-current-system = true;
+          };
+          user = {
+            profile-paths = [
+              "~/.local/state/nix/profiles/profile"
+              "/nix/var/nix/profiles/per-user/root/profile"
+            ];
+            keep-since = "1d";
+            keep-latest-n = 1;
+          };
+        };
+      };
+      timer = {
+        enable = true;
+        dates = "14d";
+      };
     };
   };
 }
