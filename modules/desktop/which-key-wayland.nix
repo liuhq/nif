109a219e26f8fd0b05bffbb5125c3ac7fcb68422
Horace Liu <im.liuhq@gmail.com>
2026-08-30 15:26:06 +0800

    feat(desktop): configure which-key-wayland

--- modules/desktop/which-key-wayland.nix
+++ modules/desktop/which-key-wayland.nix
@@ -2,16 +2,24 @@
   config,
   pkgs,
   lib,
+  myvar,
+  paths,
   inputs,
   ...
 }:
 let
   cfg = config.mymod.desktop;
+  inherit (myvar) userName;
+  inherit (paths) external;
 in
 {
   config = lib.mkIf cfg.enable {
     environment.systemPackages = [
       inputs.which-key-wayland.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
+
+    hjem.users.${userName}.xdg.config.files = {
+      "which-key-wayland/config.kdl".source = "${external}/which-key-wayland/config.kdl";
+    };
   };
 }
