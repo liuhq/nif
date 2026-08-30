9954a6ad23440213e104adfc6131dd381e9983d2
Horace Liu <im.liuhq@gmail.com>
2026-08-30 15:25:57 +0800

    feat(boot): switch greetd to noctalia greeter

--- modules/boot/greetd.nix
+++ modules/boot/greetd.nix
@@ -3,12 +3,18 @@
   config,
   pkgs,
   myvar,
+  inputs,
   ...
 }:
 let
   cfg = config.mymod.displayManager.greetd;
+  inherit (myvar) userName;
 in
 {
+  imports = [
+    inputs.noctalia-greeter.nixosModules.default
+  ];
+
   options.mymod.displayManager.greetd = {
     enable = lib.mkEnableOption "greetd + tuigreet" // {
       default = true;
@@ -16,15 +22,40 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    services.greetd = {
+    # services.greetd = {
+    #   enable = true;
+    #   settings = {
+    #     default_session = {
+    #       command = "${pkgs.tuigreet}/bin/tuigreet --cmd '${lib.getExe' pkgs.niri "niri-session"} -l' --greeting 'NixOS' --time --remember --asterisks";
+    #       user = myvar.userName;
+    #     };
+    #   };
+    #   useTextGreeter = true;
+    # };
+
+    programs.noctalia-greeter = {
       enable = true;
+
       settings = {
-        default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --cmd '${lib.getExe' pkgs.niri "niri-session"} -l' --greeting 'NixOS' --time --remember --asterisks";
-          user = myvar.userName;
+        # session = {
+        #   default = "";
+        # };
+        user.default = "${userName}";
+        appearance = {
+          scheme = "Nord";
+          hide_logo = true;
+          theme_mode = "dark";
+          corner_radius_scale = 4;
+          wallpaper = "${pkgs.my-wallpaper}/wallpaper.jpg";
         };
+        idle.timeout = 0;
+        cursor = {
+          theme = "Bocchi";
+          size = 36;
+          path = "${pkgs.bocchi-dyn-cursor}/share/icons/Bocchi";
+        };
+        keyboard.layout = "us";
       };
-      useTextGreeter = true;
     };
   };
 }
